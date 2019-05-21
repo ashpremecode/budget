ee7972332f60eb23059258873f6e8c75e35419d6
ashpremcode <ashpremecode@gmail.com>
2019-05-21 14:46:11 +1000

    second commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var budgetController = (fucntion() {
+
+})();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+	document.queryselect('.add__button').addEventListener('click', function(){
+
+		//1. Get the filed input data
+
+		//2. Add item to the budget controller
+
+		//3. Add the item to the UI
+
+		
+
+	});
+})(budgetController, UIController);
